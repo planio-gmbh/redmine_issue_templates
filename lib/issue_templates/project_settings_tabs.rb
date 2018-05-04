700ee1172085cf45d59dcb7ed1bc0ed3563ed8bd
Jens Kraemer <jk@jkraemer.net>
2018-05-04 16:29:35 +0800

    issue templates project settings
    - adds integration test case
    - adds permission to actually access settings to manage_ and edit_ permissions
    - change the way the tab is added to get lost of alias_method_chain

---
+++ lib/issue_templates/project_settings_tabs.rb
@@ -0,0 +1,18 @@
+module IssueTemplates
+  module ProjectSettingsTabs
+    def project_settings_tabs
+      super.tap do |tabs|
+        if User.current.allowed_to?(:manage_issue_templates, @project) ||
+          User.current.allowed_to?(:edit_issue_templates, @project)
+
+          @issue_templates_setting = IssueTemplateSetting.find_or_create(@project.id)
+          tabs << {
+            name: 'issue_templates',
+            partial: 'issue_templates_settings/show',
+            label: :project_module_issue_templates
+          }
+        end
+      end
+    end
+  end
+end
---
+++ test/integration/project_settings_test.rb
@@ -0,0 +1,79 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+class ProjectSettingsTest < Redmine::IntegrationTest
+  fixtures :projects, :trackers, :issue_statuses, :issues,
+           :enumerations, :users,
+           :projects_trackers,
+           :roles,
+           :member_roles,
+           :members,
+           :enabled_modules,
+           :workflows
+
+  setup do
+    @user = User.find_by_login 'admin'
+    @project = Project.find 1
+  end
+
+  def test_should_show_project_settings_if_enabled
+    log_user('admin', 'admin')
+
+    get '/projects/ecookbook/settings/issue_templates'
+    assert_response :success
+    assert_select '#content ul li a.selected', text: 'Issue templates', count: 0
+
+    @project.enabled_modules.create! name: 'issue_templates'
+    get '/projects/ecookbook/settings/issue_templates'
+    assert_response :success
+    assert_select '#content ul li a.selected', text: 'Issue templates'
+
+    assert_select 'a', text: 'Template list', href: '/projects/ecookbook/issue_templates'
+    assert_select 'a', text: 'Add template', href: '/projects/ecookbook/issue_templates/new'
+  end
+
+  def test_should_save_project_settings
+    @project.enabled_modules.create! name: 'issue_templates'
+    log_user('admin', 'admin')
+
+    setting = IssueTemplateSetting.find_or_create @project.id
+
+    patch '/projects/ecookbook/issue_templates_settings/edit',
+      setting_id: setting.id,
+      settings: {
+        inherit_templates: '1',
+        help_message: 'help message here'
+      }
+
+    assert_redirected_to '/projects/ecookbook/settings/issue_templates'
+    follow_redirect!
+    assert_response :success
+    assert_select 'textarea', text: 'help message here'
+
+    setting.reload
+    assert setting.inherit_templates?
+    assert_equal 'help message here', setting.help_message
+  end
+
+  def test_should_require_permission
+    @project.enabled_modules.create! name: 'issue_templates'
+    r = Role.find 2
+    user = User.find 3
+    log_user('jsmith', 'jsmith')
+
+    setting = IssueTemplateSetting.find_or_create @project.id
+
+    get '/projects/ecookbook/settings'
+    assert_response :success
+    assert_select '#content ul li a.selected', text: 'Issue templates', count: 0
+
+    patch '/projects/ecookbook/issue_templates_settings/edit',
+      setting_id: setting.id,
+      settings: {
+        inherit_templates: '1',
+        help_message: 'help message here'
+      }
+    assert_response 403
+  end
+
+end
+
