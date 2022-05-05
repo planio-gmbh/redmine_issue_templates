799f28d182608f9758ef9dad34e5a90e14f34438
Jens Kraemer <jk@jkraemer.net>
2022-05-05 16:32:22 +0800

    versioned migrations

--- db/migrate/0003_add_issue_title_to_issue_templates.rb
+++ db/migrate/0003_add_issue_title_to_issue_templates.rb
@@ -1,13 +1,7 @@
-class AddIssueTitleToIssueTemplates < ActiveRecord::Migration
+class AddIssueTitleToIssueTemplates < ActiveRecord::Migration[4.2]
   def self.up
     add_column :issue_templates, :issue_title, :string
-
-    IssueTemplate.reset_column_information
-    issue_templates = IssueTemplate.all
-    issue_templates.each do |t|
-      t.issue_title = t.title
-      t.save
-    end
+    connection.execute 'update issue_templates set issue_title = title'
   end
 
   def self.down
